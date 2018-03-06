1d65198bae629fcf5b1669871248776b8c68a53d
Raphael Renout <renout@MC-G-LP-C120-15.local>
2018-03-06 14:29:39 +0100

    first commit

---
+++ test/flight.js
@@ -0,0 +1,27 @@
+'use strict';
+const {expect} = require('chai');
+const supertest = require('supertest');
+const request = supertest('http://localhost:3000');
+
+describe('voyage API', function() {
+	describe('When looking for a flight CDG > BOG without a date', function() {
+		it('should return a 400 error', function() {
+			return request
+				.get('/flights')
+				.query({origin: 'CDG', destination: 'BOG'})
+				.expect(400, err => {
+					console.log(err);
+				});
+		});
+
+		it('should return a 200 for a flight CDG > BOG today', function() {
+			return request
+				.get('/flights')
+				.query({origin: 'CDG', destination: 'BOG', date: new Date()})
+				.expect(200)
+				.then(request => {
+					expect(request.body).to.be.an('array');
+				});
+		});
+	});
+});
